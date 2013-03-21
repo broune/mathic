4fc8d808bac029c663ab9cdd81ba0ffaff19bc49
Bjarke Hammersholt Roune <bjarkehr.code@gmail.com>
2013-03-21 17:35:14 +0100

    configure now downloads gtest if necessary based on --with-gtest. autogen.sh no longer downloads gtest. gtest path can be controlled with GTEST_PATH as a parameter to configure. It is now possible to build without gtest.

--- autogen.sh
+++ autogen.sh
@@ -2,25 +2,3 @@
 srcdir="`dirname '$0'`"
 
 autoreconf --verbose --install --force $srcdir
-
-# Download gtest into libs/gtest if it does not already exist. If you
-# changed gtest and need to get the original version back, just delete
-# the directory libs/gtest and run this script again.
-GTEST_DIR=$srcdir/libs/
-GTEST_TMP_DIR=$srcdir/libs/
-GTEST_VERSION=1.6.0
-GTEST_DOWNLOAD_FILE=gtest-$GTEST_VERSION.zip
-if [ ! -d libs/gtest ]; then
-  mkdir -p $GTEST_TMP_DIR;
-  rm -rf $GTEST_TMP_DIR/$GTEST_DOWNLOAD_FILE;
-  rm -rf $GTEST_TMP_DIR/gtest-$GTEST_VERSION;
-  rm -rf $GTEST_TMP_DIR/gtest;
-  ( \
-    cd $GTEST_TMP_DIR; \
-    wget http://googletest.googlecode.com/files/$GTEST_DOWNLOAD_FILE; \
-    unzip $GTEST_DOWNLOAD_FILE; \
-    rm $GTEST_DOWNLOAD_FILE \
-  )
-  rm -rf $GTEST_DIR/gtest;
-  mv $GTEST_TMP_DIR/gtest-$GTEST_VERSION $GTEST_DIR/gtest;
-fi
