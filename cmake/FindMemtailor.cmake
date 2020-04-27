f96f18604d6a1308baac3a7f14b3d8a18f636461
Mahrud Sayrafi <mahrud@berkeley.edu>
2020-04-27 03:29:39 -0500

    improve CMake build

--- cmake/FindMemtailor.cmake
+++ cmake/FindMemtailor.cmake
@@ -1,4 +1,4 @@
-# Try to find the MEMTAILOR librairies
+# Try to find the MEMTAILOR libraries
 # See https://github.com/Macaulay2/memtailor
 #
 # This file sets up GLPK for CMake. Once done this will define
@@ -10,10 +10,15 @@
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 
-find_path(MEMTAILOR_INCLUDE_DIR NAMES memtailor.h)
-find_library(MEMTAILOR_LIBRARIES NAMES memtailor libmemtailor)
+find_path(MEMTAILOR_INCLUDE_DIR NAMES memtailor.h
+  PATHS ${INCLUDE_INSTALL_DIR} ${CMAKE_INSTALL_PREFIX}/include
+  PATH_SUFFIXES memtailor
+  )
+find_library(MEMTAILOR_LIBRARIES NAMES memtailor
+  PATHS ${LIB_INSTALL_DIR} ${CMAKE_INSTALL_PREFIX}/lib
+  )
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(MEMTAILOR DEFAULT_MSG MEMTAILOR_INCLUDE_DIR MEMTAILOR_LIBRARIES)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Memtailor DEFAULT_MSG MEMTAILOR_INCLUDE_DIR MEMTAILOR_LIBRARIES)
 
 mark_as_advanced(MEMTAILOR_INCLUDE_DIR MEMTAILOR_LIBRARIES)
